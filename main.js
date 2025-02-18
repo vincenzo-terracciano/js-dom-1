e22b423bf823809fb9458ab54c812a608f1dc91f
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-18 17:58:42 +0100

    add exercise bonus

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const lightButtonEl = document.querySelector("button");
+let isLightOnOff = false;
 
 lightButtonEl.addEventListener("click", function(){
     const light = document.getElementById("lightOff")
-    light.src = "./img/yellow_lamp.png"
-    lightButtonEl.innerText = "Spegni"
+    if(isLightOnOff == false){
+        light.src = "./img/yellow_lamp.png"
+        lightButtonEl.innerText = "Spegni"
+        isLightOnOff = true;
+    } else if(isLightOnOff == true) {
+        light.src = "./img/white_lamp.png"
+        lightButtonEl.innerText = "Accendi"
+        isLightOnOff = false;
+    }
 })
